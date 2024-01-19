268a6ba4723bb0d07b702d222efa19a4868069dc
bala1802 <bala1802@live.com>
2024-01-19 06:22:18 +0530

    added quantization configuration

--- config.py
+++ config.py
@@ -7,3 +7,11 @@ DATASET = "OpenAssistant/oasst1"
 Model Configuration
 '''
 MODEL_NAME = "microsoft/phi-2"
+TRUST_REMOTE_CODE = True
+ENABLE_MODEL_CONFIG_CACHE = False
+
+'''
+Quantization Configuration
+'''
+ENABLE_4BIT = True
+QUANTIZATION_TYPE = "nf4"
---
+++ model_utils.py
@@ -0,0 +1,7 @@
+from transformers import BitsAndBytesConfig
+
+import config
+
+def construct_bits_and_bytes_config():
+
+    pass
