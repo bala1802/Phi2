2349dc5ade16f9d909bdedbb641780550fb84fa1
bala1802 <bala1802@live.com>
2024-01-19 06:30:32 +0530

    implemented script for loading the model

--- model_utils.py
+++ model_utils.py
@@ -1,7 +1,14 @@
-from transformers import BitsAndBytesConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 import config
+import quantization_utils
 
-def construct_bits_and_bytes_config():
-
-    pass
+def load_model():
+    model = AutoModelForCausalLM.from_pretrained(
+        config.MODEL_NAME,
+        quantization_config = quantization_utils.load_bits_and_bytes_config(),
+        trust_remote_code = config.TRUST_REMOTE_CODE
+    )
+    model.config.use_cache = config.ENABLE_MODEL_CONFIG_CACHE
+    return model
+    
