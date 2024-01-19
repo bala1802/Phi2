1d99df26f04b8c26405e31cbb20f21e49e4dec48
bala1802 <bala1802@live.com>
2024-01-19 07:01:12 +0530

    fixed naming convention

--- adapter_utils.py
+++ adapter_utils.py
@@ -3,7 +3,7 @@ from peft import LoraConfig
 import config
 
 def load_adapter(target_modules):
-    lora_config = LoraConfig(
+    peft_config = LoraConfig(
         lora_alpha=config.LORA_ALPHA,
         lora_dropout=config.LORA_DROPOUT,
         r = config.LORA_RANK,
@@ -11,4 +11,4 @@ def load_adapter(target_modules):
         task_type=config.TASK_TYPE,
         target_modules=target_modules
     )
-    return lora_config
+    return peft_config
