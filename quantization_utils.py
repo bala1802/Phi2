b75763e96f8a4d7e31c078f2f1164ffb19c71cbd
bala1802 <bala1802@live.com>
2024-01-19 06:25:30 +0530

    quantization util required is created

---
+++ quantization_utils.py
@@ -0,0 +1,13 @@
+import torch
+from transformers import BitsAndBytesConfig
+
+import config
+
+def construct_bits_and_bytes_config():
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=config.ENABLE_4BIT,
+        bnb_4bit_quant_type=config.QUANTIZATION_TYPE,
+        bnb_4bit_compute_dtype=torch.float16
+    )
+    
+    return bnb_config
