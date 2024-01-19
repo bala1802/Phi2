a609953d978134794c04241e03ae733a0d580a11
bala1802 <bala1802@live.com>
2024-01-19 06:34:03 +0530

    added tokenizer construction

--- model_utils.py
+++ model_utils.py
@@ -11,4 +11,9 @@ def load_model():
     )
     model.config.use_cache = config.ENABLE_MODEL_CONFIG_CACHE
     return model
-    
+
+def load_tokenizers():
+    tokenizer = AutoTokenizer.from_pretrained(
+        config.MODEL_NAME, 
+        trust_remote_code=config.TRUST_REMOTE_CODE)
+    return tokenizer
