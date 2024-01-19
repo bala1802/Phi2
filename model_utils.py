646afc3865db56f1afb52e625909e9a08d6a0887
bala1802 <bala1802@live.com>
2024-01-19 06:54:59 +0530

    added training arguments

--- model_utils.py
+++ model_utils.py
@@ -1,4 +1,4 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
 
 import config
 import quantization_utils
@@ -17,3 +17,19 @@ def load_tokenizers():
         config.MODEL_NAME, 
         trust_remote_code=config.TRUST_REMOTE_CODE)
     return tokenizer
+
+def load_training_arguments():
+    training_arguments = TrainingArguments(
+        output_dir=config.MODEL_OUTPUT_DIR,
+        per_device_train_batch_size=config.PER_DEVICE_TRAIN_BATCH_SIZE,
+        gradient_accumulation_steps=config.GRADIENT_ACCUMULATION_STEPS,
+        optim=config.OPTIM,
+        save_steps=config.SAVE_STEPS,
+        logging_steps=config.LOGGING_STEPS,
+        learning_rate=config.LEARNING_RATE,
+        fp16=config.ENABLE_FP_16,
+        max_grad_norm=config.MAX_GRAD_NORM,
+        max_steps=config.MAX_STEPS,
+        warmup_ratio=config.WARMUP_RATIO,
+        gradient_checkpointing=config.ENABLE_GRADIENT_CHECKPOINTING
+    )
